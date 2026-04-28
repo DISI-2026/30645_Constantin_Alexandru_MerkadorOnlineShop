097f77e621dfa4b7c25a5bb935a4b37938aca7cc
bdragos26 <dragosburtea@gmail.com>
2026-04-28 18:35:54 +0300

    feat: Add Docker support and update API path

--- post-service/src/main/java/org/example/postservice/controller/ReviewController.java
+++ post-service/src/main/java/org/example/postservice/controller/ReviewController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/reviews")
+@RequestMapping("/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
 
